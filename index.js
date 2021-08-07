6e45996063cb0117330efa419f853e9f4a6561db
Sirloya <87189206+Sirloya@users.noreply.github.com>
2021-08-07 19:33:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -125,15 +125,15 @@ function start(client) {
                client.sendText(message.from,' *sending* ........ it could take 1 or 2mins to send!');   
          break;
       case 'chapter4':
-             client.sendFile(message.from, 'https://drive.google.com/uc?id=1ovSHWBOAJxXivp6WhW6TA9jc1LH9PzKA&export=download' , 'chapter3.pdf', 'check this pdf', null, true)
+             client.sendFile(message.from, 'https://drive.google.com/uc?id=1ovSHWBOAJxXivp6WhW6TA9jc1LH9PzKA&export=download' , 'chapter4.pdf', 'check this pdf', null, true)
                client.sendText(message.from,' *sending* ........ it could take 1 or 2mins to send!');    
           break;  
       case 'chapter5':
-          client.sendFile(message.from, 'https://drive.google.com/uc?id=1dDMC1mDbLe8_USKcGdbY16V48b9P5CCX&export=download' , 'chapter3.pdf', 'check this pdf', null, true)
+          client.sendFile(message.from, 'https://drive.google.com/uc?id=1dDMC1mDbLe8_USKcGdbY16V48b9P5CCX&export=download' , 'chapter5.pdf', 'check this pdf', null, true)
                client.sendText(message.from,' *sending* ........ it could take 1 or 2mins to send!');   
           break;
       case 'chapter6':
-          client.sendFile(message.from, 'https://drive.google.com/uc?id=1nHx_oHLiLACb1JdptYR38Rh-FQvyvKTs&export=download' , 'chapter3.pdf', 'check this pdf', null, true)
+          client.sendFile(message.from, 'https://drive.google.com/uc?id=1nHx_oHLiLACb1JdptYR38Rh-FQvyvKTs&export=download' , 'chapter6.pdf', 'check this pdf', null, true)
                client.sendText(message.from,' *sending* ........ it could take 1 or 2mins to send!');    
           break;
       case 'chapter7':
@@ -174,16 +174,95 @@ function start(client) {
                await client.sendText(message.from, ` ✨We are on Chapter 3, Chapter 2 takpa loire! \n\n Zoom has to be  installed first! \n\n *Meeting ID* : 2087463769 \n\n *Password* : 12345 \n\n ✨Material pamba singdo hwjik fw tak pada makha da leire \n\n  /(_in case you messed up, since you are a human_  LOL!)/  \n\n *note* :✨ _if you are using gb or fm or any other unofficial whatsapp, type materials in the chat to show the menu_ ✨ \n\n✨ *Group rules* ✨  \n\n1) Respect everyone here in this group \n\n2) Sharing memes is prohibited \n\n3) Don't send unnecessary messages! \n\n✨ *Feel free to ask and discuss lessons related to biology and topics taught on class* ✨\n  _final words: if you want to show this message again, type 'menu' `
       );
    await client.sendButtons(
-          message.from,
+          event.chat,
           'Fajana Paro ko!',
           [
-            { id: 'button1', text: 'chapter2' },
-            { id: 'button2', text: 'chapter3' },
+            { id: 'button1', text: 'chapter1' },
+            { id: 'button2', text: 'chapter2' },
           ],
           'Materials',
           '-Sir Loya'
-        );       
-               break;
+        );
+            
+            
+    await client.sendButtons(
+          event.chat,
+          'Fajana Paro ko!',
+          [
+            { id: 'button3', text: 'chapter3' },
+            { id: 'button4', text: 'chapter4' },
+          ],
+          'Materials',
+          '-Sir Loya'
+        );
+         
+     await client.sendButtons(
+          event.chat,
+          'Fajana Paro ko!',
+          [  
+            
+            { id: 'button5', text: 'chapter5' },
+            { id: 'button6', text: 'chapter6' },
+          ],
+          'Materials',
+          '-Sir Loya'
+        );
+             
+    await client.sendButtons(
+          event.chat,
+          'Fajana Paro ko!',
+          [ 
+            { id: 'button7', text: 'chapter7' },
+            { id: 'button8', text: 'chapter8' },
+          ],
+          'Materials',
+          '-Sir Loya'
+        );
+          
+     await client.sendButtons(
+          event.chat,
+          'Fajana Paro ko!',
+          [ 
+            { id: 'button9', text: 'chapter9' },
+            { id: 'button10', text: 'chapter10' },
+          ],
+          'Materials',
+          '-Sir Loya'
+        );
+          
+     await client.sendButtons(
+          event.chat,
+          'Fajana Paro ko!',
+          [
+            { id: 'button11', text: 'chapter11' },
+            { id: 'button12', text: 'chapter12' },
+          ],
+          'Materials',
+          '-Sir Loya'
+        );
+            
+     await client.sendButtons(
+          event.chat,
+          'Fajana Paro ko!',
+          [
+            { id: 'button13', text: 'chapter13' },
+            { id: 'button14', text: 'chapter14' },
+          ],
+          'Materials',
+          '-Sir Loya'
+        );
+  
+     await client.sendButtons(
+          event.chat,
+          'Fajana Paro ko!',
+          [
+            { id: 'button15', text: 'chapter15' },
+            { id: 'button16', text: 'chapter16' },
+          ],
+          'Materials',
+          '-Sir Loya'
+        );    
+         break;
      
 
         
